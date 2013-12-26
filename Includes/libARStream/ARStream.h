fce9f4a4aa48922ff36edd2f82dcbd431d1b4699
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-12-26 14:50:52 +0100

    Added a generic ARStream.h header which includes all ARStream headers

---
+++ Includes/libARStream/ARStream.h
@@ -0,0 +1,15 @@
+/**
+ * @file ARStream.h
+ * @brief Global header for libARStream
+ * @date 04/12/2013
+ * @author nicolas.brulez@parrot.com
+ */
+
+#ifndef _ARSTREAM_H_
+#define _ARSTREAM_H_
+
+#include <libARStream/ARSTREAM_Error.h>
+#include <libARStream/ARSTREAM_Sender.h>
+#include <libARStream/ARSTREAM_Reader.h>
+
+#endif /* _ARSTREAM_H_ */
