09332865d8721f77cb83244f6f4152a47938a1bf
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-22 17:24:53 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: If0fc793dbd03b9d19723ac433f27436c39e7f608
    Reviewed-on: https://gerrit.parrot.biz/28576
    Reviewed-by: Nicolas Christe <nicolas.christe@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,54 +2,33 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARStream
 LOCAL_DESCRIPTION := ARSDK Stream library
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL libARNetworkAL libARNetwork
-LOCAL_EXPORT_LDLIBS := -larstream
+LOCAL_MODULE_FILENAME := libarstream.so
 
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
+LOCAL_LIBRARIES := libARSAL libARNetworkAL libARNetwork
 
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/Sources
 
-#Autotools variables
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir="" \
-	--with-libARNetworkALInstallDir="" \
-	--with-libARNetworkInstallDir=""
+LOCAL_SRC_FILES := \
+	Sources/ARSTREAM_Buffers.c \
+	Sources/ARSTREAM_NetworkHeaders.c \
+	Sources/ARSTREAM_Reader.c \
+	Sources/ARSTREAM_Reader2.c \
+	Sources/ARSTREAM_Sender.c \
+	Sources/ARSTREAM_Sender2.c \
+	gen/Sources/ARSTREAM_Error.c
 
-ifdef ARSDK_BUILD_FOR_APP
+LOCAL_INSTALL_HEADERS := \
+	Includes/libARStream/ARStream.h:usr/include/libARStream/ \
+	Includes/libARStream/ARSTREAM_Error.h:usr/include/libARStream/ \
+	Includes/libARStream/ARSTREAM_Reader.h:usr/include/libARStream/  \
+	Includes/libARStream/ARSTREAM_Reader2.h:usr/include/libARStream/ \
+	Includes/libARStream/ARSTREAM_Sender.h:usr/include/libARStream/ \
+	Includes/libARStream/ARSTREAM_Sender2.h:usr/include/libARStream/
 
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
-
-endif
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	LIBS=" -lm"
-
-endif
-
-# User define command to be launch before configure step.
-# Generates files used by configure
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
-endef
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
